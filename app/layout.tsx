96a02daf1855c2e70c24329bf60e88616c0a574f
Uvais Ahmad <uvss21@gmail.com>
2025-04-03 19:40:39 +0530

    [Update & Create SEO] Update Theme Button and Start SEO on Homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,33 @@ const inter = Inter({ subsets: ['latin'] })
 const gulzar = Gulzar({ weight: '400', subsets: ['arabic'] })
 
 export const metadata: Metadata = {
-  title: 'Sabeel-Ul-Falah - Islamic Madrasa',
-  description: 'Sabeel-Ul-Falah - An Islamic madrasa where knowledge and practice illuminate',
-}
+  title: "Sabeel ul Falah - Best Madarsa in Muzaffarnagar | Islamic Education",
+  description: "Sabeel ul Falah, led by Mufti Salim, is a top Islamic Madarsa in Muzaffarnagar, located in Madina Colony on Roorkee Road. Providing quality Quran learning and Islamic education in Uttar Pradesh.",
+  keywords: "Sabeel ul Falah, Sabeelulfalah, Mufti Salim, Madarsa in Muzaffarnagar, Islamic education, Quran learning, Roorkee Road Madarsa, Madina Colony Madarsa, Madarsa near me, Best Madarsa in UP",
+  openGraph: {
+    title: "Sabeel ul Falah - Best Islamic Madarsa in Muzaffarnagar",
+    description: "Join Sabeel ul Falah, a leading Islamic Madarsa in Muzaffarnagar, offering Quran memorization and religious education under Mufti Salim. Located in Madina Colony, Roorkee Road, Uttar Pradesh.",
+    url: "https://sabeelulfalah.com",
+    type: "website",
+    locale: "en_US",
+    images: [
+      {
+        url: "https://sabeelulfalah.com/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Sabeel ul Falah - Madarsa in Muzaffarnagar",
+      },
+    ],
+  },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   site: "@yourTwitterHandle",
+  //   title: "Sabeel ul Falah - Best Islamic Madarsa in Muzaffarnagar",
+  //   description: "Looking for an Islamic Madarsa in Muzaffarnagar? Sabeel ul Falah offers top-quality Quran and Islamic education in Madina Colony, Roorkee Road, UP.",
+  //   images: ["https://sabeelulfalah.com/og-image.jpg"],
+  // },
+};
+
 
 export default function RootLayout({
   children,
--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -10,7 +10,7 @@ export function ThemeToggle() {
   return (
     <button
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="border rounded-md w-6 h-6 flex items-center justify-center"
+      className="rounded-md w-6 h-6 flex items-center justify-center"
     >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />

3ecd77ebd12383cb619d6ac9bfcf7447b28743d3
Uvais Ahmad <uvss21@gmail.com>
2025-05-25 23:53:18 +0530

    [Dummy] Test

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-
+// Sample function to merge classes
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
